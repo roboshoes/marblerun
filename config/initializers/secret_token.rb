be2c2c7bd2b68196e630c1db85ebc98cbdfbd1e6
Mathias Paumgarten <mail@mathias-paumgarten.com>
2010-12-08 14:13:57 +0100

    Rails Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marblerun::Application.config.secret_token = '27685cb6fe0e341cadc051456952b33b19a4e17c15fb6275af56b4958c918707ab20d814d942dc6447d32cc03bf6d20e573a2228427534524368b5666a757780'
