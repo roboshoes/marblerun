5dcfbec73707e71a1cadf3721eb4304b1b667872
David Strauss <david.strausz@gmail.com>
2010-12-30 15:40:58 +0100

    removed demo files, updated .gitignore

--- demo/javascripts/utilities.js
+++
@@ -1,8 +0,0 @@
-var Utilities = {
-    degreeToRadian: function(degree) {
-        return degree * (Math.PI / 180);  
-    },
-    radianToDegree: function(radian) {
-        return radian * (180 / Math.PI);
-    }
-};
