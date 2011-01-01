320c7d8ed862c96a5fe99f3f8556edce5d996ec0
David Strauss <david.strausz@gmail.com>
2011-01-01 14:01:55 +0100

    add show views

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,4 +2,14 @@ class TracksController < ApplicationController
   def index
     
   end
+
+  def show
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render :partial => "tracks/show.json"
+      end
+    end
+  end
 end
