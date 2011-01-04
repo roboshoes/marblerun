1855c1b3a1b64fcde9f145a5e4cb654b132ecf0c
Mathias Paumgarten <mail@mathias-paumgarten.com>
2011-01-04 16:46:43 +0100

    Interface Mater Update

--- public/javascripts/marblerun/meter.js
+++ public/javascripts/marblerun/meter.js
@@ -5,16 +5,22 @@ var Meter = Class.create(DisplayObject, {
 
     this.canvas = canvas;
     this.context = canvas.getContext("2d");
-
-    this.setSize();
   },
 
   setSize: function() {
-    
+    this.canvas.width = this.canvas.height = 185;
   },
 
   draw: function() {
-    
+
+    this.setSize();
+
+    this.context.fillStyle = Pattern.meterBackground;
+    this.context.fillRect(0, 0, 185, 185);
+
+    this.context.fillStyle = Pattern.meterForeground;
+    this.context.fillRect(0, 0, 185, 185);
+
   }
 
 });
