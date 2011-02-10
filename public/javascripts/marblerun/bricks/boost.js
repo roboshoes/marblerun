aecc028d6a3b42bce5834ca2a500f11881457256
Eberhard Gräther <ebsigraether@hotmail.com>
2011-02-10 18:43:47 +0100

    improved Boost graphic

--- public/javascripts/marblerun/bricks/boost.js
+++ public/javascripts/marblerun/bricks/boost.js
@@ -8,47 +8,56 @@ var Boost = new Class.create(Brick, {
   },
 
   drawShape: function(context) {
-
+    
     // context.beginPath();
-    // context.moveTo(Brick.SIZE * 4 / 5, Brick.SIZE / 2);
-    // context.lineTo(Brick.SIZE / 5, Brick.SIZE * 9 / 10);
-    // context.lineTo(Brick.SIZE / 5, Brick.SIZE * 7/ 10);
-    // context.lineTo(Brick.SIZE / 2, Brick.SIZE / 2);
-    // context.lineTo(Brick.SIZE / 5, Brick.SIZE * 3 / 10);
-    // context.lineTo(Brick.SIZE / 5, Brick.SIZE / 10);
+    // context.moveTo(Brick.SIZE / 7, Brick.SIZE / 7);
+    // context.lineTo(Brick.SIZE * 2 / 7, Brick.SIZE / 7);
+    // context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE / 2);
+    // context.lineTo(Brick.SIZE * 2 / 7, Brick.SIZE * 6 / 7);
+    // context.lineTo(Brick.SIZE / 7, Brick.SIZE * 6 / 7);
+    // context.lineTo(Brick.SIZE * 3 / 7, Brick.SIZE / 2);
+    // context.closePath();
+    // 
+    // context.fill();
+    // 
+    // context.beginPath();
+    // context.moveTo(Brick.SIZE * 3 / 7, Brick.SIZE / 7);
+    // context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE / 7);
+    // context.lineTo(Brick.SIZE * 6 / 7, Brick.SIZE / 2);
+    // context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE * 6 / 7);
+    // context.lineTo(Brick.SIZE * 3 / 7, Brick.SIZE * 6 / 7);
+    // context.lineTo(Brick.SIZE * 5 / 7, Brick.SIZE / 2);
     // context.closePath();
     // 
     // context.fill();
     
-    
     context.beginPath();
-    context.moveTo(Brick.SIZE / 7, Brick.SIZE / 7);
-    context.lineTo(Brick.SIZE * 2 / 7, Brick.SIZE / 7);
+    context.moveTo(Brick.SIZE / 7, Brick.SIZE * 3 / 14);
+    context.lineTo(Brick.SIZE * 2 / 7, Brick.SIZE * 3 / 14);
     context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE / 2);
-    context.lineTo(Brick.SIZE * 2 / 7, Brick.SIZE * 6 / 7);
-    context.lineTo(Brick.SIZE / 7, Brick.SIZE * 6 / 7);
+    context.lineTo(Brick.SIZE * 2 / 7, Brick.SIZE * 11 / 14);
+    context.lineTo(Brick.SIZE / 7, Brick.SIZE * 11 / 14);
     context.lineTo(Brick.SIZE * 3 / 7, Brick.SIZE / 2);
     context.closePath();
     
     context.fill();
     
     context.beginPath();
-    context.moveTo(Brick.SIZE * 3 / 7, Brick.SIZE / 7);
-    context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE / 7);
+    context.moveTo(Brick.SIZE * 3 / 7, Brick.SIZE * 3 / 14);
+    context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE * 3 / 14);
     context.lineTo(Brick.SIZE * 6 / 7, Brick.SIZE / 2);
-    context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE * 6 / 7);
-    context.lineTo(Brick.SIZE * 3 / 7, Brick.SIZE * 6 / 7);
+    context.lineTo(Brick.SIZE * 4 / 7, Brick.SIZE * 11 / 14);
+    context.lineTo(Brick.SIZE * 3 / 7, Brick.SIZE * 11 / 14);
     context.lineTo(Brick.SIZE * 5 / 7, Brick.SIZE / 2);
     context.closePath();
     
     context.fill();
     
-    
     // context.strokeStyle = context.fillStyle;
     // context.lineWidth = 2;
-    
+    // 
     // context.beginPath();
-    
+    // 
     // context.moveTo(Brick.SIZE / 5, Brick.SIZE / 5);
     // context.lineTo(Brick.SIZE * 2 / 5, Brick.SIZE / 2);
     // context.lineTo(Brick.SIZE / 5, Brick.SIZE * 4 / 5);
