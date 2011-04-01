c94cf54992afbc83f13923ba53332dc37990e232
David Strauss <david.strausz@gmail.com>
2011-04-01 10:19:42 +0200

    catch options requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  #app/application_controller.rb
   after_filter :set_access_control_headers
+  before_filter :catch_options_requests
    
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
+  end
+
+  def catch_options_requests
+    render :nothing => true, :status => 200 if request.method == :options
   end
 end
