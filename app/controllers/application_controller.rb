07b8a1bfbc6a752fcc20cef89801cec29c141a4d
David Strauss <david.strausz@gmail.com>
2011-04-01 12:08:04 +0200

    enable OPTIONS request via .htaccess

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   after_filter :set_access_control_headers
-  before_filter :catch_options_requests
+  #before_filter :catch_options_requests
    
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
   end
 
-  def catch_options_requests
-    redirect_to :controller => 'tracks', :action => 'create' if request.method == :options
-  end
+  #def catch_options_requests
+  #  render :nothing => true, :status => 200 if request.method == :options
+  #end
 end
