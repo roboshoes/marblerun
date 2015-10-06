ddef78334095acf3b240a4fd23b595e876f16b32
David Strauß <david@strauss.io>
2015-10-06 09:18:49 +0200

    Move secret token to an environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Marblerun::Application.config.secret_token = '27685cb6fe0e341cadc051456952b33b19a4e17c15fb6275af56b4958c918707ab20d814d942dc6447d32cc03bf6d20e573a2228427534524368b5666a757780'
+Marblerun::Application.config.secret_token = ENV.fetch('SECRET_TOKEN')
