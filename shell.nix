4eea1b717f07ac72d4d0f37ea629f184b47c2f6d
David Strauß <david@strauss.io>
2020-11-16 13:45:22 +0100

    Use correct Bundler version

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
-{ pkgs ? import (fetchGit {
-  url = https://github.com/NixOS/nixpkgs-channels;
-  ref = "3590ff2d4c64711a194e3f18fc5c140e7dfa25df";
-}) {} }:
+{
+  pkgs ? import (fetchGit {
+    url = https://github.com/NixOS/nixpkgs-channels;
+    ref = "3590ff2d4c64711a194e3f18fc5c140e7dfa25df";
+  }) {},
+  ruby ? pkgs.ruby_2_3,
+  bundler ? pkgs.bundler.override { inherit ruby; }
+
+}:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    ruby_2_3
+    ruby
     bundler
   ];
   shellHook = ''
@@ -14,7 +19,5 @@ pkgs.mkShell {
     mkdir -p .nix-gems
     export GEM_HOME=$PWD/.nix-gems
     export GEM_PATH=$GEM_HOME
-
-    echo "Welcome to your MARBLERUN Environment"
   '';
 }
